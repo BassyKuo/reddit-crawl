af2fc03b52ab10d9fa6fa7f1ccd55ce6c0b20628
BassyKuo <aaammmyyy27@gmail.com>
2016-12-04 07:46:34 +0800

    crawl.py v1.5

--- crawl.py
+++ crawl.py
@@ -1,36 +1,48 @@
 import	urllib,urllib2
 import	json
 import	requests
-from pprint import pprint
+import	datetime
+#from pprint import pprint
+
+import os
+import sys
 
 """
 Reddit Articles Object
 """
 class RedditObj:
-	def __init__ (self, url):
-		self.url = url
-		self.url2JSON(url)
-		self.dic = {}
+	def __init__ (self, domain='http://www.reddit.com/r/nosleep/', count=0, name=''):
 		"""
 		dic = {
-				"$username_1" : {
-					"$article_id_1" : { 'title' , 'link_flair_text' , 'score' , 'ups' , 
-										'created_utc' , 'edited' , 'num_comments' , 'selftext' , 
-										'media' , 'over_18' , 'domain' , 'subreddit' , 'url' , 'name' },
-					"$article_id_2" : {...},
-					},
-				"$username_2" : {
-					"$article_id_1" : {...},
-					"$article_id_2" : {...},
-					....
-					},
-				"$username_3"...
+				'domain' : 'http://www.reddit.com/r/nosleep/', 
+				'next_count' : 75,
+				'next_name' : 'ttrr4'
+				'next_url' : 'http://www.reddit.com/r/nosleep/.json?count=75&after=ttrr4'
+				'articles' : {
+					"$username_1" : {
+						"$article_id_1" : { 'title' , 'link_flair_text' , 'score' , 'ups' , 
+											'created_utc' , 'edited' , 'num_comments' , 'selftext' , 
+											'media' , 'over_18' , 'domain' , 'subreddit' , 'url' , 'name' },
+						"$article_id_2" : {...},
+						},
+					"$username_2" : {
+						"$article_id_1" : {...},
+						"$article_id_2" : {...},
+						....
+						},
+					"$username_3"...
+					}
 				}
 		"""
+		self.domain = domain
+		self.next_count = count
+		self.next_name = name
+		self.next_url = self.domain + ".json?count=" + str(self.next_count) + "&after=" + self.next_name
+		self.dic = {'domain':self.domain, 'next_count':self.next_count, 'next_name':self.next_name, 'next_url':self.next_url, 'articles':{}}
 		self.key_list = ['title', 'link_flair_text', 'score', 'ups', 'created_utc', 'edited', 'num_comments', 'selftext', 'media', 'over_18', 
 						 'domain', 'subreddit', 'url', 'name']
-		
-	def url2JSON (self, url):
+
+	def url2json (self, url):
 		self.url = url
 		request = urllib2.Request(url)
 		request.add_header( "User-Agent",         
@@ -39,19 +51,19 @@ class RedditObj:
 		self.contents = json.loads(respond.read())
 		self.count = len(self.contents['data']['children'])
 		self.after = self.contents['data']['after']
-		#print self.contents
-		#print self.count
-		#print self.after
 		return self.contents
 
-	def count (self):
-		return self.count
-
-	def after (self):
-		return self.after
-	
-	def dic (self):
-		return self.dic
+	def next_page (self):
+		try:
+			count = self.next_count + 25
+			name = self.after
+			url = self.domain + ".json?count=" + str(count) + "&after=" + name
+		except TypeError:
+			return -1
+		self.next_count = count
+		self.next_name = name
+		self.next_url = url
+		return self.next_url
 
 	def get_content (self, start=0, end=0):
 		if end == 0:
@@ -60,8 +72,8 @@ class RedditObj:
 			print "Error: start < `end' <= count"
 			return -1
 		dic = {}
-		print "start: " +  str(start)
-		print "end: " + str(end)
+		#print "start: " +  str(start)
+		#print "end: " + str(end)
 		for num in range(start, end):
 			author = self.contents['data']['children'][num]['data']['author']
 			a_id = self.contents['data']['children'][num]['data']['id']
@@ -69,42 +81,68 @@ class RedditObj:
 			dic[author][a_id] = {}
 			for k in self.key_list:
 				dic[author][a_id][k] = self.contents['data']['children'][num]['data'][k]
-			print dic
+			#print dic
+		self.update(dic)
 		return dic
 
 	def writeJSON(self, filename="reddit.json", dic={}):
+		self.dic['next_count'] = self.next_count
+		self.dic['next_name'] = self.next_name
+		self.dic['next_url'] = self.domain + ".json?count=" + str(article.next_count) + "&after=" + article.next_name
 		if dic=={}:
 			dic = self.dic
-		with open(filename, 'a') as outfile:
+		with open(filename, 'w') as outfile:
 			json.dump(dic, outfile, ensure_ascii=True)
+		outfile.close()
 
 	def readJSON(self, filename="reddit.json"):
 		with open(filename, 'r') as infile:
-			dic = json.loads(infile.read())
-		return dic
+			self.dic = json.loads(infile.read())
+		self.domain = self.dic['domain']
+		self.next_count = self.dic['next_count']
+		self.next_name = self.dic['next_name']
+		self.next_url = self.dic['next_url']
+		infile.close()
+		return self.dic
 
 	def update (self, new_dic):
 		for author in new_dic.keys():
-			if self.dic.has_key(author) == False:
-				self.dic[author] = {}
-			self.dic[author].update(new_dic[author])
+			if self.dic['articles'].has_key(author) == False:
+				self.dic['articles'][author] = {}
+			self.dic['articles'][author].update(new_dic[author])
 
 
 if __name__ == '__main__':
-	count = 0
-	domain = "https://www.reddit.com/r/nosleep/"
-	url = domain + ".json"
-	article = RedditObj(url)
-	dic = article.get_content(0,article.count)
-	article.update(dic)
+	year  = datetime.date.today().timetuple()[0]
+	month = datetime.date.today().timetuple()[1]
+	day   = datetime.date.today().timetuple()[2]
+	week  = datetime.date.today().isoweekday()
+	calendar_week = datetime.date.today().isocalendar()[1]
+	program = sys.argv[0]
+	domain = sys.argv[1]
+	pages = sys.argv[2]
+	#filename = os.path.join('data', "reddit_%s_%02d%02d.json" % (domain.split('/')[4], month, day))
+	filename = os.path.join('data', "reddit_%s.json" % (domain.split('/')[4]))
 
-	for n in range(1,30):
-		count += 25
-		url = domain + ".json?count=" + str(count) + "&after=" + article.after
-		article.url2JSON(url)
-		dic = article.get_content(0,article.count)
-		article.update(dic)
-	article.writeJSON("reddit_1203.json")
+	if os.path.isdir('data') is False:
+		os.makedirs('data')
+
+	if os.path.exists(filename) is True:
+		article = RedditObj()
+		article.readJSON(filename)
+	else:
+		domain = "https://www.reddit.com/r/nosleep/"
+		article = RedditObj(domain, 0, '')
+
+	for n in range(0,int(pages)):
+		article.url2json(article.next_url)
+		article.get_content(0,article.count)
+		nurl = article.next_page()
+		if nurl == -1:
+			break
+		print nurl
+		
+	article.writeJSON(filename)
 	
 
 
