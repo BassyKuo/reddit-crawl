522890fc442b4aff38c94bcf4d39eb5a876f357c
BassyKuo <aaammmyyy27@gmail.com>
2016-12-05 03:29:00 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,25 @@
+import	urllib,urllib2
+import	json
+import	requests
+import	datetime
+#from pprint import pprint
+
+from crawl import RedditObj
+
+filename ='data/reddit_nosleep_1204.json'
+article_old = RedditObj()
+article_old.readJSON(filename)
+#print article_old.dic['articles']
+
+obj = RedditObj('http://www.reddit.com/r/nosleep/', 0 ,'')
+obj.url2json(obj.next_url)
+obj.get_content(0,obj.count)
+obj.next_page()
+print "===New keys==="
+print obj.dic['articles'].keys()
+print "===Old keys==="
+print article_old.dic['articles'].keys()
+
+print "===Update keys==="
+article_old.update(obj.dic['articles'])
+print article_old.dic['articles'].keys()
